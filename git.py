0137d437be139719230fcc6b4f2117ec13614ce6
Bibek <bibek.joshi91@gmail.com>
2023-05-03 08:37:02 +0545

    first commit

---
+++ git.py
@@ -0,0 +1,10 @@
+#git init
+# git add .
+# git commit -m "<commit_message>"
+
+# Setting git configuration
+#1. git config --global user.name "<username>"
+#2. git config --global user.email "<email>"
+
+
+# git status #Shows the current status of all git staged and
